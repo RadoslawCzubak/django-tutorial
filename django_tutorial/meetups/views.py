b8ab0631e9a32fac45b3e03e959e9f8369ed8b0b
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2021-06-22 22:25:34 +0200

    add first endpoint and some django template language examples

--- django_tutorial/meetups/views.py
+++ django_tutorial/meetups/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    meetups = [
+        {'title': "MyFirstMeetup"},
+        {'title': "MySecondMeetup"}
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': True,
+        'meetups': meetups
+    })
